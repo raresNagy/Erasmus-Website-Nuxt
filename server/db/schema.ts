6584505483e1ea17532bbf59b0f669f18a71a72d
raresNagy <rares.nagy@gmail.com>
2024-03-08 16:42:38 +0200

    commit

---
+++ server/db/schema.ts
@@ -0,0 +1,33 @@
+import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
+//eclaring enum in database
+
+export const users = sqliteTable("users", {
+	id: integer("id").primaryKey().notNull(),
+	name: text("name").notNull(),
+	passwordHash: text("passwordHash", { length: 64 }).notNull().unique(),
+});
+
+export const images = sqliteTable("images", {
+	id: integer("id").primaryKey().notNull(),
+	filePath: text("filePath"),
+});
+
+export const challenges = sqliteTable("challeges", {
+	id: integer("id").primaryKey(),
+	requitement: text("requirement"),
+});
+
+export type User = typeof users.$inferSelect; // return type when queried
+export type InsertUser = typeof users.$inferInsert; // insert type
+
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import Database from "better-sqlite3";
+
+const sqlite = new Database("sqlite.db");
+const db = drizzle(sqlite);
+
+const result: User[] = db.select().from(users).all();
+
+const insertUser = (user: InsertUser) => {
+	return db.insert(users).values(user).run();
+};
